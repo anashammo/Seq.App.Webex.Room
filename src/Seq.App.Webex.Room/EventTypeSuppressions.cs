e5d97b919650b42030fd76233b38983836a49f8d
Anas Hammo <eng.anasmohad@gmail.com>
2024-11-19 02:36:21 +0300

    Implement Suppress

---
+++ src/Seq.App.Webex.Room/EventTypeSuppressions.cs
@@ -0,0 +1,40 @@
+namespace Seq.App.Webex.Room;
+
+public class EventTypeSuppressions
+{
+    private readonly Dictionary<uint, DateTime> _suppressions = new Dictionary<uint, DateTime>();
+    private readonly int _suppressionMinutes;
+
+    public EventTypeSuppressions(int suppressionMinutes)
+    {
+        _suppressionMinutes = suppressionMinutes;
+    }
+
+    public bool ShouldSuppressAt(uint eventType, DateTime utcNow)
+    {
+        if (_suppressionMinutes == 0)
+            return false;
+
+        if (!_suppressions.TryGetValue(eventType, out var suppressedSince) ||
+            suppressedSince.AddMinutes(_suppressionMinutes) < utcNow)
+        {
+            // Not suppressed, or suppression expired
+
+            // Clean up old entries
+            var expired = _suppressions.FirstOrDefault(kvp => kvp.Value.AddMinutes(_suppressionMinutes) < utcNow);
+            while (expired.Value != default)
+            {
+                _suppressions.Remove(expired.Key);
+                expired = _suppressions.FirstOrDefault(kvp => kvp.Value.AddMinutes(_suppressionMinutes) < utcNow);
+            }
+
+            // Start suppression again
+            suppressedSince = utcNow;
+            _suppressions[eventType] = suppressedSince;
+            return false;
+        }
+
+        // Suppressed
+        return true;
+    }
+}
--- src/Seq.App.Webex.Room/WebexApp.cs
+++ src/Seq.App.Webex.Room/WebexApp.cs
@@ -1,7 +1,6 @@
 using System.Text.Json;
 using Seq.Api;
 using Seq.Api.Model.Events;
-using Seq.App.Webex.Room.Models;
 using Seq.Apps;
 using Seq.Apps.LogEvents;
 
@@ -10,23 +9,76 @@ namespace Seq.App.Webex.Room;
 [SeqApp("Webex Room", Description = "Send events and notifications to a Webex configured Room")]
 public class WebexApp : SeqApp, ISubscribeToAsync<LogEventData>
 {
-    [SeqAppSetting] public string RoomId { get; set; }
+    [SeqAppSetting(
+        DisplayName = "Webex Room Id",
+        IsOptional = false,
+        InputType = SettingInputType.Text,
+        HelpText = "The Id used to send the events to a Webex room")]
+    public string RoomId { get; set; }
 
-    [SeqAppSetting] public string AuthorizationBearerToken { get; set; }
+    [SeqAppSetting(
+        DisplayName = "Authorization Bearer Token",
+        IsOptional = false,
+        InputType = SettingInputType.Password,
+        HelpText = "The Authorization Bearer token of the Bot used to send the events to a Webex room")]
+    public string AuthorizationBearerToken { get; set; }
 
-    [SeqAppSetting] public string ApiKey { get; set; }
+    [SeqAppSetting(
+        DisplayName = "Seq API Key",
+        IsOptional = false,
+        InputType = SettingInputType.Text,
+        HelpText = "The API Key used to get event details within an alert using Seq API")]
+    public string ApiKey { get; set; }
+    
+    [SeqAppSetting(
+        DisplayName = "Use HTTP Proxy",
+        IsOptional = false,
+        InputType = SettingInputType.Checkbox,
+        HelpText = "If checked, HTTP proxy will be used to connect to the Webex API")]
+    public bool UseProxy { get; set; }
 
-    [SeqAppSetting] public string UseProxy { get; set; }
+    [SeqAppSetting(
+        DisplayName = "HTTP Proxy Address",
+        IsOptional = true,
+        InputType = SettingInputType.Text,
+        HelpText = "The HTTP Proxy Address used to connect to the Webex API")] 
+    public string ProxyAddress { get; set; }
 
-    [SeqAppSetting] public string ProxyAddress { get; set; }
+    [SeqAppSetting(
+        DisplayName = "HTTP Proxy Port",
+        IsOptional = true,
+        InputType = SettingInputType.Integer,
+        HelpText = "The HTTP Proxy Port used to connect to the Webex API")] 
+    public int ProxyPort { get; set; }
 
-    [SeqAppSetting] public string ProxyPort { get; set; }
+    [SeqAppSetting(
+        DisplayName = "Secure HTTP Proxy",
+        IsOptional = false,
+        InputType = SettingInputType.Checkbox,
+        HelpText = "If checked, The HTTP proxy will be used to connect to the Webex API will use username and password")]
+    public string AuthenticatedProxy { get; set; }
 
-    [SeqAppSetting] public string AuthenticatedProxy { get; set; }
+    [SeqAppSetting(
+        DisplayName = "HTTP Proxy Username",
+        IsOptional = true,
+        InputType = SettingInputType.Text,
+        HelpText = "The HTTP Proxy Username used to connect to the Webex API")] 
+    public string ProxyUsername { get; set; }
 
-    [SeqAppSetting] public string ProxyUsername { get; set; }
-
-    [SeqAppSetting] public string ProxyPassword { get; set; }
+    [SeqAppSetting(
+        DisplayName = "HTTP Proxy Password",
+        IsOptional = true,
+        InputType = SettingInputType.Password,
+        HelpText = "The HTTP Proxy Password used to connect to the Webex API")] 
+    public string ProxyPassword { get; set; }
+    
+    [SeqAppSetting(
+        DisplayName = "Suppression time (minutes)",
+        IsOptional = true,
+        HelpText = "Once an event type has been sent to Webex, the time to wait before sending again, The default is zero")]
+    public int SuppressionMinutes { get; set; }
+    
+    private EventTypeSuppressions _suppressions;
 
     public WebexApp()
     {
@@ -44,6 +96,13 @@ public class WebexApp : SeqApp, ISubscribeToAsync<LogEventData>
 
         try
         {
+            _suppressions = _suppressions ?? new EventTypeSuppressions(SuppressionMinutes);
+            
+            if (_suppressions.ShouldSuppressAt(evt.EventType, DateTime.UtcNow))
+            {
+                return;
+            }
+            
             var eventAsJson = JsonSerializer.Serialize(evt, new JsonSerializerOptions());
 
             var eventAsJsonDocument = JsonDocument.Parse(eventAsJson);
